37eba9d71a7c7aecf28107a3bba6bed3192e17c7
octalpixel <mithushancj@gmail.com>
2024-12-05 19:47:42 +0530

    base

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  "https://mowgzyrspfshtgzbhshx.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vd2d6eXJzcGZzaHRnemJoc2h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM0MDYyMzQsImV4cCI6MjA0ODk4MjIzNH0.BY5008h0OU-TqLB_q3ndhnAQjegap4dsHTWbeVAg8Yw"
 ) 
